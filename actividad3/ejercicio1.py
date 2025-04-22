09262f906ab98b57789f87690167c7f1425121c7
cristopherSantana6 <cristopherrafael644@gmail.com>
2025-04-22 07:32:12 -0600

    calculo de promedio

---
+++ actividad3/ejercicio1.py
@@ -0,0 +1,10 @@
+calificacion1=int(input("Ingrese la calificación 1: "))
+calificacion2=int(input("Ingrese la calificación 2: "))
+calificacion3=int(input("Ingrese la calificación 3: "))
+suma=calificacion1+calificacion2+calificacion3
+promedio=suma/3
+
+print(f"""calificacion 1: {calificacion1}
+calificacion 2: {calificacion2}
+calificacion 3: {calificacion3}
+promedio: {promedio:.0f}""")
